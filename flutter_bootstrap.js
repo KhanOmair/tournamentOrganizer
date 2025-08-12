6cfd71341d204177154cff36633a4ea7451a8ba0
Omair Khan <knightomair@gmail.com>
2025-08-12 13:33:42 -0700

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1295250685"
+    serviceWorkerVersion: "713761302"
   }
 });
